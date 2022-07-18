bed0c119eb295b3de2ee5b65ab9df899c45d2e69
Patrick Hayo <patrick.hayo@email.de>
2022-07-18 18:46:25 +0200

    updated

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
-/**
- ## OUTPUT
-**/
+output "ids" {
+  description = "Contains a list of the the identity id of the identities"
+  value       = { for identity in azurerm_user_assigned_identity.identity : identity.name => identity.id }
+}
+
+output "principal_ids" {
+  description = "Contains a list of the the identity principal_id of the identities"
+  value       = { for identity in azurerm_user_assigned_identity.identity : identity.name => identity.principal_id }
+}
+
+output "client_ids" {
+  description = "Contains a list of the the identity client_id  of the identities"
+  value       = { for identity in azurerm_user_assigned_identity.identity : identity.name => identity.client_id }
+}
+
